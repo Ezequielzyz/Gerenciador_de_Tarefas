973b71f10b136eabe9f1000e2888dcb8a0064011
Ezequiel <eb66963@gmail.com>
2026-09-18 16:57:16 -0300

    Rotas

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const taskform = document.getElementById('task-form')
-const taskinput = document.getElementById('task-input')
-const tasklist = document.getElementById('task-list')
-
-const li = document.createElement('li')
